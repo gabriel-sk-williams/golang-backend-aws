3b1f94556ffc0d94cf0df5b070f3ef69ea466464
Gabriel Williams <gabers@NANTO.PRISON>
2023-01-12 21:17:09 -0800

    minor edit eb

--- application.go
+++ application.go
@@ -7,7 +7,7 @@ import (
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path == "/" {
-		fmt.Fprintf(w, "Hello World! Append a name to the URL to say hello. For example, use %s/Mary to say hello to Mary.", r.Host)
+		fmt.Fprintf(w, "Hello World! append meeee reeee For example, use %s/Mary to say hello to Mary.", r.Host)
 	} else {
 		fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 	}
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module hello
 
 go 1.19
+
+require (
+	github.com/aws/aws-sdk-go v1.44.176 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+)
